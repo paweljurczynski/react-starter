a84c7b5bd4f1ae4eae948f805d01d1e472041872
Pawel <pawel.jurczynski@live.com>
2018-06-30 09:42:24 +0200

    Add basic configuration

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# react-starter
+# React Starter
+
+## Instalation
+
+1. Clone repository
+    ```
+    git clone https://github.com/paweljurczynski/react-starter.git
+    ```
+2. Go to `react-starter`
+    ```angular2html
+    cd react-starter
+    ```
+3. Install dependencies
+    ```
+    yarn
+    ```
+    
+## How to run a project?
+To run project just type in command line
+```
+yarn dev
+```
+
+## How to build a project? 
+To build a project type in command line
+```
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const webpack = require('webpack');
+const env = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    context: path.resolve(__dirname, 'src/'),
+    entry: './index.tsx',
+    devtool: env === 'development' ? 'cheap-module-source-map' : undefined,
+    output: {
+        path: path.resolve(__dirname, 'build/'),
+        filename: 'main.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'static/'),
+        hot: true,
+        hotOnly: true,
+        inline: true,
+        port: 8080,
+        clientLogLevel: 'warning'
+    },
+    mode: env,
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js', 'json' ],
+        modules: [
+            path.resolve(__dirname, './src'),
+            path.resolve(__dirname, './node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'build')
+                ],
+                use: [
+                    { loader: 'awesome-typescript-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'head'
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defer: 'main.js'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
